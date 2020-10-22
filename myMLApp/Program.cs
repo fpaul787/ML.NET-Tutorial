948842eeb3a3e3edeff45544b11ab4295123c888
Frantz Paul <fpaul787@gmail.com>
2020-10-22 01:31:50 -0400

    first commit

---
+++ myMLApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyMLAppML.Model;
+using System;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "That is rude.";
+
+            // Load model and preict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
